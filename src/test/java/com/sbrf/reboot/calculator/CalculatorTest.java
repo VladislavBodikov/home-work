b57a21dbab023e215f0c69ac49e1607e40ab03b1
krullerr@yandex.ru <krullerr@yandex.ru>
2021-12-23 21:32:02 +0300

    creating tests for Calculator

--- src/test/java/com/sbrf/reboot/calculator/CalculatorTest.java
+++ src/test/java/com/sbrf/reboot/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package com.sbrf.reboot.calculator;
 
+import com.sbrf.reboot.Calculator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -26,6 +27,18 @@ class CalculatorTest {
         assertEquals(3, new Calculator().getDivision(9, 3));
     }
 
+    @Test
+    void getExponentiation(){
+        assertEquals(8,new Calculator().getExponentiation(2,3));
+    }
+    @Test
+    void getSquareRoot(){
+        assertEquals(5,new Calculator().getSquareRoot(25));
+    }
+    @Test
+    void getSin(){
+        assertEquals(1.0/2,new Calculator().getSin(30));
+    }
     @Test
     void classHasSevenMethods() {
         assertEquals(7, Calculator.class.getMethods().length - Object.class.getMethods().length);

09b1527d2b3beeffb3f8a7cb5b7c0d19729e5cd9
krullerr@yandex.ru <krullerr@yandex.ru>
2022-01-09 13:18:25 +0300

    lesson-4 create FileAccountRepository base homework

--- src/main/java/com/sbrf/reboot/AccountService.java
+++ src/main/java/com/sbrf/reboot/AccountService.java
@@ -4,7 +4,7 @@ import com.sbrf.reboot.repository.AccountRepository;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
-import java.util.Set;
+import java.io.IOException;
 
 @RequiredArgsConstructor
 public class AccountService {
@@ -12,16 +12,16 @@ public class AccountService {
     @NonNull
     private AccountRepository accountRepository;
 
-    public boolean isClientHasContract(long clientId, long contractNumber) {
+    public boolean isClientHasContract(long clientId, long contractNumber) throws IOException {
         return accountRepository.getAllAccountsByClientId(clientId).contains(contractNumber);
     }
 
-    public long getClientIdByContractNumber(long contractNumber) {
-        Set<Long> allAccounts = accountRepository.getAllAccounts();
-        return allAccounts
-                .stream()
-                .filter((x) -> accountRepository.getAllAccountsByClientId(x).contains(contractNumber))
-                .findFirst()
-                .get();
-    }
+//    public long getClientIdByContractNumber(long contractNumber) {
+//        Set<Long> allAccounts = accountRepository.getAllAccounts();
+//        return allAccounts
+//                .stream()
+//                .filter((x) -> accountRepository.getAllAccountsByClientId(x).contains(contractNumber))
+//                .findFirst()
+//                .get();
+//    }
 }

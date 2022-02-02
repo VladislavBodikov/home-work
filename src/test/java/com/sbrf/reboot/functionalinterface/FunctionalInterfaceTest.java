f269afd5b78b962663b5c0def29696e7133ca234
krullerr@yandex.ru <krullerr@yandex.ru>
2022-02-02 16:39:05 +0300

    lesson-10 5+ exercise : add 4 test for custom lambda-operations

--- src/test/java/com/sbrf/reboot/functionalinterface/FunctionalInterfaceTest.java
+++ src/test/java/com/sbrf/reboot/functionalinterface/FunctionalInterfaceTest.java
@@ -7,6 +7,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -63,6 +65,70 @@ public class FunctionalInterfaceTest {
 
         Assertions.assertTrue(strings.contains("{\"objectName\":\"Object-1\"}"));
         Assertions.assertTrue(strings.contains("{\"objectName\":\"Object-2\"}"));
+    }
+
+    @FunctionalInterface
+    interface StringOp {
+        String func(String s);
+    }
+    static String doWithString(StringOp stringOp, String s){
+        return stringOp.func(s);
+    }
+
+    @Test
+    @DisplayName("Удалить каждую четную букву")
+    void removeOddTest(){
+        StringOp stringOp = (s)->{
+            String result = "";
+            for (int i = 0; i < s.length(); i++){
+                result += (i % 2 == 0) ? "" : s.charAt(i);
+            }
+            return result;
+        };
+
+        Assertions.assertEquals(doWithString(stringOp,"собака"),"оаа");
+    }
+    @Test
+    @DisplayName("Перевернуть строку")
+    void reverseStringTest(){
+        StringOp stringOp = (s)->{
+            String result = "";
+            for (int i = s.length() - 1; i >= 0 ; i--){
+                result += s.charAt(i);
+            }
+            return result;
+        };
+
+        Assertions.assertEquals(doWithString(stringOp,"собака"),"акабос");
+    }
+
+    @FunctionalInterface
+    interface CustomCheck<T>{
+        boolean func(T t);
+    }
+    static boolean makeOperation(CustomCheck customCheck, Integer t){
+        return customCheck.func(t);
+    }
+    @Test
+    @DisplayName("Является ли четным")
+    void isOdd(){
+        CustomCheck<Integer> customCheck = (x)-> (x % 2 == 0);
+        Assertions.assertTrue(makeOperation(customCheck,100));
+    }
+    @Test
+    @DisplayName("Является ли квадратом")
+    void isSquare(){
+        CustomCheck<Integer> customCheck = (x)-> {
+            for (int i = 0; i < x; i++){
+                if (i * i == x)
+                    return true;
+            }
+            return false;
+        };
+        Assertions.assertAll(
+                ()->Assertions.assertTrue(makeOperation(customCheck,100)),
+                ()->Assertions.assertFalse(makeOperation(customCheck,99)));
+
     }
 
 }
